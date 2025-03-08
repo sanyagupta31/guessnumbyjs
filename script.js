eb1c237758f49cd619cef6a611de0e0f931f9f87
sanyagupta31 <160328740+sanyagupta31@users.noreply.github.com>
2025-03-08 11:25:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded",function(){
     let attempt=3;
     while(attempt>0){
         let guess=parseInt(prompt("guess a number between 1 and 10:"));
-        if (guess=secretnum) {
+        if(isNaN(guess)){
+            alert("Invalid input! Please enter a number.");
+            continue;
+        }
+        if (guess===secretnum) {
             alert("congratulations! you guessed it right.");
             break;
     }
     else {
         attempt--;
         if(attempt>0){
+            let hint = guess>secretnum?"too high!":"too low!";
             alert(` wrong guess! try again.you have ${attempt} attempts left.`);
 
         }
